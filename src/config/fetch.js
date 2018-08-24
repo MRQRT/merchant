53db4697825e412c01da3b088beea6dddeb48982
guojiawei <guojiawei@32gold.com>
2018-08-24 19:40:00 +0800

    去掉日志

--- src/config/fetch.js
+++ src/config/fetch.js
@@ -22,7 +22,6 @@ axios.interceptors.request.use((config) => {
     if(config.method === 'get') {
         config.params = qs.stringify(config.data)
     }
-    console.log(store.getters)
     if(store.getters.token) {
         config.headers['accessToken'] = store.getters.token// 让每个请求携带token
     }
