df83c3b07bfbbe4f098c9097e26239442909bf8c
guojiawei <guojiawei@32gold.com>
2018-11-16 15:33:11 +0800

    pull

--- config/index.js
+++ config/index.js
@@ -21,7 +21,7 @@ module.exports = {
     },
     host:'10.88.88.24',//comp guo
     // host:'192.168.1.113',//comp guo
-    host: '10.88.88.8', // can be overwritten by process.env.HOST
+    // host: '10.88.88.8', // can be overwritten by process.env.HOST
     // host: '192.168.1.181', // can be overwritten by process.env.HOST
     port: 8082, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
